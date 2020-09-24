1c113f94f1ad0c6004e34b131a40524c572b556f
nero1342 <heathcliff1342@gmail.com>
2020-09-24 12:40:25 +0700

    Merge branch 'khoa' of https://github.com/nero1342/CS426-MidtermProject-FindFind into nero
    # Conflicts:
    #	app/src/debug/res/values/google_maps_api.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/apcs/nero/findfind/MapsActivity.java

--- app/src/main/java/com/apcs/nero/findfind/MainActivity.java
+++ app/src/main/java/com/apcs/nero/findfind/MainActivity.java
@@ -4,18 +4,33 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_EDIT_INFO = 0x9345;
     ImageButton _btnCreateRoom, _btnJoinRoom, _btnEditInfo;
     Infomation _user;
+    LinearLayout _layoutJoinRoom;
+    GridLayout _layoutMenu;
+    BackendRoom _backendRoom;
+
+    TextView _textViewStatus;
+
+    Integer isProcess = new Integer(0);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,24 +39,106 @@ public class MainActivity extends AppCompatActivity {
         initComponents();
     }
 
+    public void showKeyboard(){
+        InputMethodManager inputMethodManager = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
+
+    public void closeKeyboard(){
+        InputMethodManager inputMethodManager = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+    }
+
     private void initComponents() {
+        _textViewStatus = (TextView) findViewById(R.id.txtTemp);
+        _layoutMenu = (GridLayout) findViewById(R.id.layoutMenu);
+        _layoutJoinRoom = (LinearLayout) findViewById(R.id.layoutJoinRoom);
+        _layoutJoinRoom.setVisibility(View.INVISIBLE);
+
+        _backendRoom = new BackendRoom(getApplicationContext(), _textViewStatus, MainActivity.this, isProcess);
+
         _user = new Infomation("Nguyen E Ro", new LocationInfo(new LatLong(10.8029884,106.679072), "419 Phan Xich Long, Phuong 3, Phu Nhuan, HCM"));
         // Create Room
         _btnCreateRoom = (ImageButton) findViewById(R.id.btnCreateRoom);
+
         _btnCreateRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RoomActivity.class);
-                intent.putExtra("user", _user);
-                startActivity(intent);
+                // Create new room with roomID
+                isProcess = 1;
+                _backendRoom.createRoomIDFromServer();
             }
         });
+        _textViewStatus.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int roomID = Integer.parseInt(s.toString());
+                switch (isProcess) {
+                    case 2: //
+                        isProcess = 0;
+                        if (roomID == -1) {
+                            Toast.makeText(getApplicationContext(), "This room is unavailable. Please check it again.", Toast.LENGTH_SHORT).show();
+                        }
+                    case 1: // Create new room
+                        isProcess = 0;
+                        if (roomID != -1) {
+                            Intent intent = new Intent(MainActivity.this, RoomActivity.class);
+                            intent.putExtra("user", _user);
+                            intent.putExtra("roomID", roomID);
+                            intent.putExtra("backendRoomIP", _backendRoom.getIP_ADDRESS());
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Can't create a new room", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+        //
         // Join Room
         _btnJoinRoom = (ImageButton) findViewById(R.id.btnJoinRoom);
         _btnJoinRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Coming soon...", Toast.LENGTH_LONG).show();
+                _layoutJoinRoom.setVisibility(View.VISIBLE);
+                _layoutMenu.setVisibility(View.INVISIBLE);
+                EditText editText = findViewById(R.id.editTextJoinRoomID);
+                editText.requestFocus();
+                showKeyboard();
+                //Toast.makeText(getApplicationContext(), "Coming soon...", Toast.LENGTH_LONG).show();
+            }
+        });
+        Button _btnCancelJoinRoom = (Button) findViewById(R.id.btnCancelJoinRoom);
+        _btnCancelJoinRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _layoutMenu.setVisibility(View.VISIBLE);
+                _layoutJoinRoom.setVisibility(View.INVISIBLE);
+                closeKeyboard();
+            }
+        });
+        Button _btnJoinRoom2 = (Button) findViewById(R.id.btnJoinRoom2);
+        _btnJoinRoom2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextRoomID = (EditText) findViewById(R.id.editTextJoinRoomID);
+                try {
+                    isProcess = 2;
+                    int roomID = Integer.parseInt(editTextRoomID.getText().toString());
+                    _backendRoom.checkValidRoom(roomID);
+                }
+                catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Please input correct roomID", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
--- app/src/main/java/com/apcs/nero/findfind/RoomActivity.java
+++ app/src/main/java/com/apcs/nero/findfind/RoomActivity.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -13,19 +18,32 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 
 public class RoomActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE_FIND_BEST_LOCATION = 0x9333;
+    private static final int REQUEST_CODE_FIND_BEST_PLACE = 0x9333;
     private static final int REQUEST_CODE_ADD_ROOMMATE = 0x9345;
-    private static final int REQUEST_CODE_ADD_LOCATION = 0x9332;
+    private static final int REQUEST_CODE_ADD_PLACE = 0x9332;
     private ArrayList<Infomation> _roommates = new ArrayList<>();
-    private ArrayList<Infomation> _locations = new ArrayList<>();
-    private AdapterInfomation _adapterRoommates, _adapterLocations;
+    private ArrayList<Infomation> _places = new ArrayList<>();
+    private AdapterInfomation _adapterRoommates, _adapterPlaces;
     private ListView _listviewRoommates = null;
-    private ListView _listviewLocations = null;
+    private ListView _listviewPlaces = null;
+    private int _roomID;
+    private BackendRoom _backendRoom;
+    private Infomation _user;
+    private TextView _textViewStatus;
+    private int isProcess = 0;
+
+    Handler _handler = new Handler();
+    private Runnable _runnableUpdateData;
+    private String _versionMem, _versionLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,32 +55,44 @@ public class RoomActivity extends AppCompatActivity {
     }
 
     private void loadData() {
+        _textViewStatus = new TextView(this);
+
         Intent intent = getIntent();
         Infomation current_user = (Infomation) intent.getSerializableExtra("user");
-        _roommates.add(current_user);
+        String BACKEND_IP_ADDRESS = (String)intent.getSerializableExtra("backendRoomIP");
+        _backendRoom = new BackendRoom(BACKEND_IP_ADDRESS, getApplicationContext(), _textViewStatus, RoomActivity.this, isProcess);
 
+        _roomID = (int) intent.getIntExtra("roomID", -1);
+        _user = current_user;
+        //
+        _roommates.add(current_user);
+        isProcess = _backendRoom.ADD_ROOMMATE;
+        _backendRoom.addItem(_user, _roomID, "mem");
+        //
     }
+
     private void updateListviewRoommates() { _adapterRoommates.notifyDataSetChanged(); }
-    private void updateListviewLocations() {
-        _adapterLocations.notifyDataSetChanged();
+    private void updateListviewPlaces() {
+        _adapterPlaces.notifyDataSetChanged();
     }
 
     AdapterView.OnItemClickListener remove_when_click_roommates = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             if (position == 0) {
-                Toast.makeText(getApplicationContext(), "Can not remove myself", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Can not remove anyone", Toast.LENGTH_LONG).show();
                 return;
             }
             _roommates.remove(position);
             updateListviewRoommates();
         }
     };
-    AdapterView.OnItemClickListener remove_when_click_locations = new AdapterView.OnItemClickListener() {
+    AdapterView.OnItemClickListener remove_when_click_places = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            _locations.remove(position);
-            updateListviewLocations();
+            _places.remove(position);
+            Toast.makeText(getApplicationContext(), "Remove place at position " + ((Integer)position).toString() + " successfully", Toast.LENGTH_LONG).show();
+            updateListviewPlaces();
         }
     };
     private void initComponents() {
@@ -70,29 +100,31 @@ public class RoomActivity extends AppCompatActivity {
         _adapterRoommates = new AdapterInfomation(this, R.layout.listview_items, _roommates);
         _listviewRoommates.setAdapter(_adapterRoommates);
 
-        _listviewLocations = findViewById(R.id.listLocation);
-        _adapterLocations = new AdapterInfomation(this, R.layout.listview_items, _locations);
-        _listviewLocations.setAdapter(_adapterLocations);
+        _listviewPlaces = findViewById(R.id.listLocation);
+        _adapterPlaces = new AdapterInfomation(this, R.layout.listview_items, _places);
+        _listviewPlaces.setAdapter(_adapterPlaces);
 
         _listviewRoommates.setOnItemClickListener(remove_when_click_roommates);
-        _listviewLocations.setOnItemClickListener(remove_when_click_locations);
+        _listviewPlaces.setOnItemClickListener(remove_when_click_places);
+
         ImageButton btnAddRoommate = (ImageButton) findViewById(R.id.btnAddRoommate);
         btnAddRoommate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RoomActivity.this, InfomationActivity.class);
-                intent.putExtra("title", "Add New Roommate");
-                startActivityForResult(intent, REQUEST_CODE_ADD_ROOMMATE);
+                Toast.makeText(getApplicationContext(), "This feature is temporaryly locked in this version.", Toast.LENGTH_LONG).show();
+//                Intent intent = new Intent(RoomActivity.this, InfomationActivity.class);
+//                intent.putExtra("title", "Add New Roommate");
+//                startActivityForResult(intent, REQUEST_CODE_ADD_ROOMMATE);
             }
         });
 
-        ImageButton btnAddLocation = (ImageButton) findViewById(R.id.btnAddLocation);
-        btnAddLocation.setOnClickListener(new View.OnClickListener() {
+        ImageButton btnAddPlace = (ImageButton) findViewById(R.id.btnAddLocation);
+        btnAddPlace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RoomActivity.this, InfomationActivity.class);
-                intent.putExtra("title", "Add New Location");
-                startActivityForResult(intent, REQUEST_CODE_ADD_LOCATION);
+                intent.putExtra("title", "Add New Place");
+                startActivityForResult(intent, REQUEST_CODE_ADD_PLACE);
             }
         });
 
@@ -105,15 +137,139 @@ public class RoomActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "There is no user in this room", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if (_locations.size() == 0) {
-                    Toast.makeText(getApplicationContext(), "There is no candidate location", Toast.LENGTH_LONG).show();
+                if (_places.size() == 0) {
+                    Toast.makeText(getApplicationContext(), "There is no candidate place", Toast.LENGTH_LONG).show();
                     return;
                 }
                 intent.putExtra("people",_roommates);
-                intent.putExtra("location", _locations);
-                startActivityForResult(intent, REQUEST_CODE_FIND_BEST_LOCATION);
+                intent.putExtra("location", _places);
+                startActivityForResult(intent, REQUEST_CODE_FIND_BEST_PLACE);
             }
         });
+
+        TextView textViewRoomID = (TextView) findViewById(R.id.textViewRoomID);
+        textViewRoomID.setText(((Integer)_roomID).toString());
+
+
+        // Used to be live status when asynchronous volley completed. Data will be stored inside _textViewStatus
+        _textViewStatus.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String data = s.toString();
+                switch (isProcess) {
+                    case BackendRoom.ADD_PLACE: {
+                        isProcess = 0;
+                        break;
+                    }
+                    case BackendRoom.ADD_ROOMMATE: {
+                        _user.setID(Integer.parseInt(data));
+                        isProcess = 0;
+                        break;
+                    }
+                    case BackendRoom.REMOVE_PLACE: {
+                        isProcess = 0;
+                        break;
+                    }
+                    case BackendRoom.REMOVE_ROOMMATE: {
+                        isProcess = 0;
+                        break;
+                    }
+                    case BackendRoom.UPDATE_ROOMMATE: {
+                        try {
+                            JSONArray jsonArray = new JSONArray(data);
+                            _roommates.clear();
+                            for (int i = 0; i < (int)jsonArray.length(); ++i) {
+                                Infomation info = new Infomation(jsonArray.getJSONObject(i));
+                                _roommates.add(info);
+                            }
+                            updateListviewRoommates();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        _handler.postDelayed(_runnableUpdateData, 0); // first run instantly
+                        isProcess = 0;
+                        break;
+                    }
+                    case BackendRoom.UPDATE_PLACE: {
+                        try {
+                            JSONArray jsonArray = new JSONArray(data);
+                            _places.clear();
+                            for (int i = 0; i < (int)jsonArray.length(); ++i) {
+                                Infomation info = new Infomation(jsonArray.getJSONObject(i));
+                                _places.add(info);
+                            }
+                            updateListviewPlaces();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        _handler.postDelayed(_runnableUpdateData, 0); // first run instantly
+                        isProcess = 0;
+                        break;
+                    }
+                    case BackendRoom.CHECK_UPDATE_ROOMMATE: {
+                        if (!data.equals(_versionMem)) {
+                            // Stop check
+                            _handler.removeCallbacks(_runnableUpdateData);
+                            //
+
+                            isProcess = BackendRoom.UPDATE_ROOMMATE;
+                            _versionMem = data;
+                            _backendRoom.get(_roomID, "mem");
+                        }
+
+                        break;
+                    }
+                    case BackendRoom.CHECK_UPDATE_PLACE: {
+                        if (!data.equals(_versionLoc)) {
+                            // Stop check
+                            _handler.removeCallbacks(_runnableUpdateData);
+                            //
+                            isProcess = BackendRoom.UPDATE_PLACE;
+                            _versionLoc = data;
+                            _backendRoom.get(_roomID, "loc");
+                        }
+                        break;
+                    }
+                }
+            }
+        });
+
+        _runnableUpdateData = new Runnable() {
+            @Override
+            public void run() {
+                _handler.postDelayed(this, 2 * 1000); // every 2 seconds
+                checkUpdateDataFromServer();
+            }
+        };
+        _handler.postDelayed(_runnableUpdateData, 5 * 1000); // first run instantly
+    }
+    int countCheckUpdate = 0;
+    private void checkUpdateDataFromServer() {
+        countCheckUpdate ^= 1;
+        String type = "";
+        if (countCheckUpdate == 0) {
+            isProcess = BackendRoom.CHECK_UPDATE_ROOMMATE;
+            type = "mem";
+        } else {
+            isProcess = BackendRoom.CHECK_UPDATE_PLACE;
+            type = "loc";
+        }
+        _backendRoom.version(_roomID, type);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        isProcess = BackendRoom.REMOVE_ROOMMATE;
+        _backendRoom.removeItem(_user.getID(), "mem");
+        _handler.removeCallbacks(_runnableUpdateData);
+        super.onBackPressed();
     }
 
     @Override
@@ -131,11 +287,14 @@ public class RoomActivity extends AppCompatActivity {
                 }
                 break;
             }
-            case REQUEST_CODE_ADD_LOCATION: {
+            case REQUEST_CODE_ADD_PLACE: {
                 if (resultCode == Activity.RESULT_OK) {
                     Infomation newLocation = (Infomation) data.getSerializableExtra("user");
-                    _locations.add(newLocation);
-                    updateListviewLocations();
+                    _places.add(newLocation);
+                    updateListviewPlaces();
+                    //
+                    isProcess = BackendRoom.ADD_PLACE;
+                    _backendRoom.addItem(newLocation, _roomID, "loc");
                 } else {
                     Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
                 }
